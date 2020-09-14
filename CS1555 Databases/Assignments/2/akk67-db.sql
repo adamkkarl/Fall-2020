6c5b7c1bdbf89693af1bf972a1a3d0b7e03b4a8c
adamkkarl <adamkkarl@gmail.com>
2020-09-13 20:42:48 -0400

    start assignment 2

---
+++ CS1555 Databases/Assignments/2/akk67-db.sql
@@ -0,0 +1,46 @@
+/*
+  Adam Karl
+  akk67
+  CS1555
+  Assignment #2
+ */
+
+
+/* 1.
+ TECH_PERSONNEL
+ arity: 6
+ cardinality: 0
+
+ USERS
+ arity: 5
+ cardinality: 0
+
+ CATEGORIES
+ arity: 3
+ cardinality: 0
+
+ INVENTORY
+ arity: 5
+ cardinality: 0
+
+ LOCATIONS
+ arity: 4
+ cardinality: 0
+
+ TICKETS
+ arity: 9
+ cardinality: 0
+
+ ASSIGNMENTS
+ arity: 4
+ cardinality: 0
+ */
+
+/* 2. */
+DROP TABLE IF EXISTS TECH_PERSONNEL;
+DROP TABLE IF EXISTS USERS;
+DROP TABLE IF EXISTS CATEGORIES;
+DROP TABLE IF EXISTS INVENTORY;
+DROP TABLE IF EXISTS LOCATIONS;
+DROP TABLE IF EXISTS TICKETS;
+DROP TABLE IF EXISTS ASSIGNMENTS;
