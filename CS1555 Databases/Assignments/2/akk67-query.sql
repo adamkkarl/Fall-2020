6cbcd3be4e1e148416f02f44923ff35f833a5d74
adamkkarl <adamkkarl@gmail.com>
2020-09-15 16:01:54 -0400

    queries finished

--- CS1555 Databases/Assignments/2/akk67-query.sql
+++ CS1555 Databases/Assignments/2/akk67-query.sql
@@ -5,3 +5,14 @@
   Assignment #2
  */
 
+/* 6 */
+/* a */
+
+SELECT ticket_number, description
+FROM TICKETS
+WHERE date_closed IS NOT NULL;
+
+/* b */
+SELECT ticket_number, description
+FROM TICKETS
+WHERE machine_name='kaso.cs.pitt.edu'
