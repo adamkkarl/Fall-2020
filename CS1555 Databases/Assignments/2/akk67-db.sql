b81a4523eb83992e84c26baac6850c20a9bda45d
adamkkarl <adamkkarl@gmail.com>
2020-09-15 14:12:25 -0400

    added information up to TICKETS table

--- CS1555 Databases/Assignments/2/akk67-db.sql
+++ CS1555 Databases/Assignments/2/akk67-db.sql
@@ -149,3 +149,146 @@ ADD COLUMN supervisor int DEFAULT 1110001;
 /* e */
 ALTER TABLE TICKETS
 ALTER COLUMN days_worked_on SET DEFAULT 0;
+
+
+/* 4 */
+INSERT INTO TECH_PERSONNEL
+VALUES (1110001, 'Bob', 'Hoffman', 'bh1', 'Unix systems', '412-624-8404', 15);
+INSERT INTO TECH_PERSONNEL
+VALUES (1110002, 'Terry', 'Wood', 'tw1', 'Hardware', '412-624-8831', 14);
+INSERT INTO TECH_PERSONNEL
+VALUES (1110003, 'Russ', 'Howard', 'rh1', 'Windows Systems', '412-624-8834', 9);
+INSERT INTO TECH_PERSONNEL
+VALUES (1110004, 'Christian', 'Mason', 'cm1', 'Web Technology', '412-624-9181', 5);
+
+INSERT INTO USERS
+VALUES (5550001, 'Shenoda', 'Guirguis', 'shg18', '412-624-8443');
+INSERT INTO USERS
+VALUES (5550002, 'Lory', 'Almoakar', 'la1', '412-624-8443');
+INSERT INTO USERS
+VALUES (5550003, 'Thao', 'Pham', 'tp1', '412-624-8850');
+INSERT INTO USERS
+VALUES (5550004, 'Michel', 'Hanna', 'mh1', '412-624-8402');
+
+INSERT INTO CATEGORIES
+VALUES (1, 'Printing Problem', 'problems have to do with printers and printing')
+INSERT INTO CATEGORIES
+VALUES (2, 'Network Problem', 'problems have to do with the network and/or internet');
+INSERT INTO CATEGORIES
+VALUES (3, 'Windows OS problem', 'problems have to do with Windows OS maintenance');
+INSERT INTO CATEGORIES
+VALUES (4, 'Unix/Linux OS problem', 'problems have to do with Unix/Linux OS maintenance');
+INSERT INTO CATEGORIES
+VALUES (5, 'Hardware Problem', 'HW maintenance issues');
+INSERT INTO CATEGORIES
+VALUES (6, 'Software Problem', 'SW maintenance issues');
+INSERT INTO CATEGORIES
+VALUES (7, 'Server Problem', 'Any problem has to do with the dept. servers');
+INSERT INTO CATEGORIES
+VALUES (8, 'Software install', 'Requests to install additional SW');
+INSERT INTO CATEGORIES
+VALUES (9, 'Web Problem', 'Any problem has to do with the dept. web sites');
+INSERT INTO CATEGORIES
+VALUES (10, 'Windows Help', 'Questions about Windows (hotline support)');
+INSERT INTO CATEGORIES
+VALUES (11, 'Unix/Linux Help', 'Questions about Unix/Linux (hotline support)');
+INSERT INTO CATEGORIES
+VALUES (12, 'SW Help', 'Questions about installed SW (hotline support)');
+INSERT INTO CATEGORIES
+VALUES (13, 'AFS problem', 'Problems related to the AFS system.');
+
+INSERT INTO LOCATIONS
+VALUES (1, '6th floor', 'SENSQ', 'db research lab');
+INSERT INTO LOCATIONS
+VALUES (2, '6th floor', 'SENSQ', 'os research lab');
+INSERT INTO LOCATIONS
+VALUES (3, '5th floor', 'SENSQ', 'arch research lab');
+INSERT INTO LOCATIONS
+VALUES (4, '6th floor', 'SENSQ', 'java lab');
+INSERT INTO LOCATIONS
+VALUES (5, '5th floor', 'SENSQ', 'reading room');
+
+INSERT INTO INVENTORY
+VALUES ('acta.cs.pitt.edu', '102.168.222.226', 'sq6-52', '01:23:45:67:89:ab', 1);
+INSERT INTO INVENTORY
+VALUES ('kaly.cs.pitt.edu', '102.168.222.227', 'sq5-14', '01:23:45:67:89:ac', 3);
+INSERT INTO INVENTORY
+VALUES ('elements.cs.pitt.edu', '102.168.222.228', 'sq6-23', '01:23:45:67:89:ad', 5);
+INSERT INTO INVENTORY
+VALUES ('kaso.cs.pitt.edu', '102.168.222.229', 'sq6-22', '01:23:45:67:89:ae', 2);
+INSERT INTO INVENTORY
+VALUES ('oxgyn.cs.pitt.edu', '102.168.212.216', 'sq5-17', '01:23:45:67:89:af', 3);
+INSERT INTO INVENTORY
+VALUES ('rodi.cs.pitt.edu', '102.168.212.217', 'sq6-51', '01:23:45:67:89:ba', 1);
+INSERT INTO INVENTORY
+VALUES ('java lab machine', '102.168.212.220', 'sq6-41', '01:23:45:67:89:bb', 4);
+
+INSERT INTO TICKETS
+VALUES (000000567841, 5550001,
+        TO_DATE('12-20-2019', 'MM-DD-YYYY'), TO_DATE('12-28-2019', 'MM-DD-YYYY'),
+        9, 5, 'acta.cs.pitt.edu', 'The fan is noisy.');
+INSERT INTO TICKETS
+VALUES (000000567842, 5550002,
+        TO_DATE('12-20-2019', 'MM-DD-YYYY'), TO_DATE('12-22-2019', 'MM-DD-YYYY'),
+        3, 1, 'kaly.cs.pitt.edu', 'Can not print any PDF file!');
+INSERT INTO TICKETS
+VALUES (000000567843, 5550002,
+        TO_DATE('12-23-2019', 'MM-DD-YYYY'), NULL,
+        NULL, 6, 'elements.cs.pitt.edu', 'Seems that the server is down?!');
+INSERT INTO TICKETS
+VALUES (000000567844, 5550001,
+        TO_DATE('12-28-2019', 'MM-DD-YYYY'), TO_DATE('12-31-2019', 'MM-DD-YYYY'),
+        4, 6, 'acta.cs.pitt.edu', 'The AFS client crashes all the time!');
+INSERT INTO TICKETS
+VALUES (000000567845, 5550003,
+        TO_DATE('1-1-2020', 'MM-DD-YYYY'), TO_DATE('1-5-2020', 'MM-DD-YYYY'),
+        5, 4, 'kaso.cs.pitt.edu', 'The mouse responds in a weird way.');
+INSERT INTO TICKETS
+VALUES (000000567846, 5550002,
+        TO_DATE('1-2-2020', 'MM-DD-YYYY'), TO_DATE('1-2-2020', 'MM-DD-YYYY'),
+        1, 2, 'oxgyn.cs.pitt.edu', 'can not connect to the oxgyn server!');
+INSERT INTO TICKETS
+VALUES (000000567847, 5550001,
+        TO_DATE('1-5-2020', 'MM-DD-YYYY'), TO_DATE('1-6-2020', 'MM-DD-YYYY'),
+        2, 8, 'acta.cs.pitt.edu', 'I failed to login to the evaluation site.');
+INSERT INTO TICKETS
+VALUES (000000567848, 5550003,
+        TO_DATE('1-5-2020', 'MM-DD-YYYY'), TO_DATE('1-12-2020', 'MM-DD-YYYY'),
+        1, 2, 'kaso.cs.pitt.edu', 'May I have SigmaPlot installed on my machine?');
+INSERT INTO TICKETS
+VALUES (000000567849, 5550003,
+        TO_DATE('1-13-2020', 'MM-DD-YYYY'), NULL,
+        NULL, 5, 'kaso.cs.pitt.edu', 'Some keys on the keyboard does not function.');
+INSERT INTO TICKETS
+VALUES (000000567850, 5550004,
+        TO_DATE('1-14-2020', 'MM-DD-YYYY'), NULL,
+        NULL, 10, 'rodi.cs.pitt.edu', 'The sound card is unrecognized.');
+INSERT INTO TICKETS
+VALUES (000000567851, 5550001,
+        TO_DATE('1-14-2020', 'MM-DD-YYYY'), TO_DATE('1-23-2020', 'MM-DD-YYYY'),
+        10, 5, 'acta.cs.pitt.edu', 'The harddrive got bad sector.');
+INSERT INTO TICKETS
+VALUES (000000567852, 5550003,
+        TO_DATE('1-14-2020', 'MM-DD-YYYY'), TO_DATE('1-17-2020', 'MM-DD-YYYY'),
+        4, 3, 'kaso.cs.pitt.edu', 'Windows crashed!');
+INSERT INTO TICKETS
+VALUES (000000567853, 5550003,
+        TO_DATE('1-15-2020', 'MM-DD-YYYY'), TO_DATE('1-21-2020', 'MM-DD-YYYY'),
+        7, 12, 'kaso.cs.pitt.edu', 'can not reach AFS-home directory from home.');
+INSERT INTO TICKETS
+VALUES (000000567854, 5550001,
+        TO_DATE('1-16-2020', 'MM-DD-YYYY'), TO_DATE('1-17-2020', 'MM-DD-YYYY'),
+        2, 6, 'acta.cs.pitt.edu ', 'I have trouble configuring Outlook.');
+INSERT INTO TICKETS
+VALUES (000000567855, 5550003,
+        TO_DATE('1-20-2020', 'MM-DD-YYYY'), NULL,
+        NULL, 3, 'kaso.cs.pitt.edu', 'Windows crashed!');
+INSERT INTO TICKETS
+VALUES (000000567856, 5550004,
+        TO_DATE('1-25-2020', 'MM-DD-YYYY'), NULL,
+        NULL, 8, 'java lab machine', 'Can not submit my h/w using the submission site.');
+
+
+
+
+
