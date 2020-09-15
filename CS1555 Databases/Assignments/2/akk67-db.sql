da5cf32c99b2b50d8bcb3da3a025d55286f6a66a
adamkkarl <adamkkarl@gmail.com>
2020-09-15 13:17:02 -0400

    finished adding tables

--- CS1555 Databases/Assignments/2/akk67-db.sql
+++ CS1555 Databases/Assignments/2/akk67-db.sql
@@ -37,10 +37,96 @@
  */
 
 /* 2. */
-DROP TABLE IF EXISTS TECH_PERSONNEL;
-DROP TABLE IF EXISTS USERS;
-DROP TABLE IF EXISTS CATEGORIES;
+DROP TABLE IF EXISTS ASSIGNMENTS;
+DROP TABLE IF EXISTS TICKETS;
 DROP TABLE IF EXISTS INVENTORY;
 DROP TABLE IF EXISTS LOCATIONS;
-DROP TABLE IF EXISTS TICKETS;
-DROP TABLE IF EXISTS ASSIGNMENTS;
+DROP TABLE IF EXISTS CATEGORIES;
+DROP TABLE IF EXISTS USERS;
+DROP TABLE IF EXISTS TECH_PERSONNEL;
+
+/* assume first and last name are 20 chars or fewer */
+CREATE TABLE TECH_PERSONNEL (
+    pplSoft int NOT NULL,
+    fname   varchar(20) NOT NULL,
+    lname   varchar(20) NOT NULL,
+    pittID  varchar(10),
+    expertise   varchar(20),
+    office_phone    char(12),
+    CONSTRAINT TECH_PERSONNEL_pk PRIMARY KEY (pplSoft)
+);
+
+/* assume first and last name are 20 chars or fewer */
+CREATE TABLE USERS (
+    pplSoft int NOT NULL,
+    fname   varchar(20) NOT NULL,
+    lname   varchar(20) NOT NULL,
+    pittID  varchar(10),
+    office_phone    char(12),
+    CONSTRAINT USERS_pk PRIMARY KEY (pplSoft)
+);
+
+/* assume category names are 30 chars or fewer */
+CREATE TABLE CATEGORIES (
+    category_id int NOT NULL,
+    category    varchar(30),
+    description varchar(280),
+    CONSTRAINT CATEGORIES_pk PRIMARY KEY (category_id)
+);
+
+/* assume location names are 20 chars or fewer */
+/* assume building names are 10 chars or fewer */
+CREATE TABLE LOCATIONS (
+    location_id int NOT NULL,
+    location    varchar(20),
+    building    varchar(10),
+    notes   varchar(280),
+    CONSTRAINT LOCATIONS_pk PRIMARY KEY (location_id)
+);
+
+/* assume machine names are 30 chars or fewer */
+/* assume network port names are 10 chars or fewer */
+CREATE TABLE INVENTORY (
+    machine_name    varchar(30),
+    ip_address  varchar(20),
+    network_port    varchar(10),
+    mac_address varchar(20) NOT NULL,
+    location_id int,
+    CONSTRAINT INVENTORY_pk PRIMARY KEY (mac_address),
+    CONSTRAINT INVENTORY_loc_fk FOREIGN KEY (location_id) REFERENCES LOCATIONS
+                       ON UPDATE CASCADE
+);
+
+/* assume machine names are 30 chars or fewer */
+CREATE TABLE TICKETS (
+    ticket_number   int NOT NULL,
+    owner_pplSoft   int NOT NULL,
+    date_submitted  date,
+    date_closed date,
+    days_worked_on  int,
+    category_id int NOT NULL,
+    machine_name    varchar(30),
+    location    int, /* =================== */
+    description varchar(280),
+    CONSTRAINT TICKETS_pk PRIMARY KEY (ticket_number),
+    CONSTRAINT TICKETS_loc_fk FOREIGN KEY (location) REFERENCES LOCATIONS (location_id)
+                      ON UPDATE CASCADE,
+    CONSTRAINT TICKETS_category_fk FOREIGN KEY (category_id) REFERENCES CATEGORIES (category_id)
+                      ON UPDATE CASCADE,
+    CONSTRAINT TICKETS_owner_fk FOREIGN KEY (owner_pplSoft) REFERENCES USERS (pplSoft)
+                      ON UPDATE CASCADE
+);
+
+CREATE TABLE ASSIGNMENTS (
+    ticket_number   int,
+    tech_pplSoft    int,
+    date_assigned   date,
+    status  varchar(20),
+    CONSTRAINT ASSIGNMENTS_pk PRIMARY KEY (ticket_number),
+    CONSTRAINT ASSIGNMENTS_ticket_fk FOREIGN KEY (ticket_number) REFERENCES TICKETS
+                          ON UPDATE CASCADE,
+    CONSTRAINT ASSIGNMENTS_tech_pplsoft_fk FOREIGN KEY (tech_pplSoft) REFERENCES TECH_PERSONNEL (pplSoft)
+                          ON UPDATE CASCADE
+);
+
+/* 3 */
